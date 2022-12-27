5420cb285162cfdf75d0b9db3deb52f86e913ed2
Dmitrii <dimau777@gmail.com>
2022-12-27 14:57:59 +0400

    Positions of tasks in the list can be changed

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,35 @@ import { DragDropContext, DropResult } from "react-beautiful-dnd";
 export function App() {
   const [state, setState] = useState<IState>(initialData);
 
-  const handleOnDragEnd = useCallback((result: DropResult) => {}, []);
+  const handleOnDragEnd = (result: DropResult) => {
+    const { source, destination, draggableId } = result;
+    if (!destination) return;
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    const column = state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    setState(newState);
+  };
 
   const res = state.columnOrder.map((columnId) => {
     const column = state.columns[columnId];
