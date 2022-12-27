378a1e5304498befb9a8f26f2f5389a81cef84a7
Dmitrii <dimau777@gmail.com>
2022-12-27 20:27:15 +0400

    Add changes colors when dragging one of Draggable elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,32 @@ import React, { useCallback, useState } from "react";
 import { initialData } from "./initialDate";
 import { Column } from "./Column";
 import { IState } from "./interfaces";
-import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  DragStart,
+  DragUpdate,
+  DropResult,
+} from "react-beautiful-dnd";
 
 export function App() {
   const [state, setState] = useState<IState>(initialData);
 
+  const handleOnDragStart = (start: DragStart) => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 200ms ease";
+  };
+
+  const handleOnDragUpdate = (update: DragUpdate) => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity}`;
+  };
+
   const handleOnDragEnd = (result: DropResult) => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
     const { source, destination, draggableId } = result;
     if (!destination) return;
 
@@ -43,5 +63,13 @@ export function App() {
     return <Column key={column.id} column={column} tasks={tasks} />;
   });
 
-  return <DragDropContext onDragEnd={handleOnDragEnd}>{res}</DragDropContext>;
+  return (
+    <DragDropContext
+      onDragStart={handleOnDragStart}
+      onDragUpdate={handleOnDragUpdate}
+      onDragEnd={handleOnDragEnd}
+    >
+      {res}
+    </DragDropContext>
+  );
 }
